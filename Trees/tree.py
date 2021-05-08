9170fd09e12c961d48f48eeec78fbbe87236836a
Samkit Shah <shahsamkit35@gmail.com>
2021-05-08 20:15:02 +0530

    traversing

--- Trees/tree.py
+++ Trees/tree.py
@@ -1,5 +1,23 @@
 class TreeNode:
-    def  __init__(self,data,children=[]):
+    def __init__(self,data,children=[]):
         self.data=data
         self.children=children
-root=TreeNode("Menu")    
+    def addChild(self,child):
+        self.children.append(child)
+Root = TreeNode("menu",[])
+Hot_drinks=TreeNode("Hot Drinks",[])
+Cold_drinks=TreeNode("Cold Drinks",[])
+Root.addChild(Hot_drinks)
+Root.addChild(Cold_drinks)
+tea=TreeNode("Tea",[])
+coffee=TreeNode("Coffee",[])
+sprite=TreeNode("Sprite",[])
+thumbs_up=TreeNode("Thumbs Up",[])
+Hot_drinks.addChild(tea)
+Hot_drinks.addChild(coffee)
+Cold_drinks.addChild(sprite)
+Cold_drinks.addChild(thumbs_up)
+
+#prints Hot_drinks child 
+for i in Hot_drinks.children:
+    print(i.data)
