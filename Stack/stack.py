bb99dc7208aa564c678dfa5ea4221e1674527d08
Samkit Shah <shahsamkit35@gmail.com>
2021-06-24 23:05:07 +0530

    isEmpty Function added in Stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -4,15 +4,24 @@ class Stack:
         self.stk=[]
     #function to push the data
     def push(self,data):
-        return self.stk.append(data)
+        self.stk.append(data)
     # function to pop the data
     def pop(self):
         return self.stk.pop()
+    #To check wheter the stack is empty or not
+    def isEmpty(self):
+        if self.stk == None:
+            return "Stack is Empty"
+        else:
+            return "Stack is not Empty"         
     
 
 plates = Stack()
+
 plates.push("Plate A")
 plates.push("Plate B")
 plates.push("Plate C")   
 
 plates.pop()
+
+print(plates.isEmpty())
